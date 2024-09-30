c1fa760d073afac03c0ac8e8fc95932a9dab8808
BJCreslin <bjcreslin@gmail.com>
2024-09-30 20:19:31 +0700

    добавил  склонение слова

--- src/main/kotlin/ru/bjcreslin/naidizakupku/telegram/events/handlers/impl/CodeBotService.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/telegram/events/handlers/impl/CodeBotService.kt
@@ -15,8 +15,21 @@ class CodeBotService(
     override fun execute(chatId: Long, params: String): String {
         val user = telegramUserService.getNewOrSavedUserByTelegramId(chatId)
         val code = telegramCodeService.createCode(user)
+        val minutes = code.actionTimeMinutes
         val codeText =
-            """Код для входа в сервис ${code.code}, действует ${code.actionTimeMinutes} минут.""".trimIndent()
+            """Код для входа в сервис ${code.code}, действует ${minutes} ${getMinuteWord(minutes)}.""".trimIndent()
         return codeText
+    }
+
+    fun getMinuteWord(minutes: Long): String {
+        val lastDigit = minutes % 10
+        val lastTwoDigits = minutes % 100
+
+        return when {
+            lastTwoDigits in 11..14 -> "минут"
+            lastDigit.toInt() == 1 -> "минута"
+            lastDigit in 2..4 -> "минуты"
+            else -> "минут"
+        }
     }
 }
