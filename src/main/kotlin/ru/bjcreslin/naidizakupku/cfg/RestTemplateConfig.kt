8702ad0e18b6f1b94414187d65eaf565f6b33164
BJCreslin <bjcreslin@gmail.com>
2025-05-10 15:08:30 +0700

    изменил RestTemplateConfig

--- src/main/kotlin/ru/bjcreslin/naidizakupku/cfg/RestTemplateConfig.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/cfg/RestTemplateConfig.kt
@@ -2,7 +2,7 @@ package ru.bjcreslin.naidizakupku.cfg
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 
 @Configuration
@@ -10,8 +10,9 @@ class RestTemplateConfig {
 
     @Bean
     fun restTemplate(): RestTemplate {
-        val factory = HttpComponentsClientHttpRequestFactory()
+        val factory = SimpleClientHttpRequestFactory()
         factory.setConnectTimeout(5000)
+        factory.setReadTimeout(5000)
         return RestTemplate(factory)
     }
 }
