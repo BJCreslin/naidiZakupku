11b9c0e319a31b4700d2606b787436b8f86309cc
BJCreslin <bjcreslin@gmail.com>
2025-05-25 18:05:50 +0700

    Добпаил актуатор

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("io.jsonwebtoken:jjwt:$jsonwebtoken")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
