380881e62d27ba00e515ac9ee0fc7961c1b103d0
BJCreslin <bjcreslin@gmail.com>
2025-05-25 17:07:51 +0700

    Изменил repository

--- src/main/kotlin/ru/bjcreslin/naidizakupku/security/repository/RoleRepository.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/security/repository/RoleRepository.kt
@@ -1,10 +1,12 @@
 package ru.bjcreslin.naidizakupku.security.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.stereotype.Repository
 import ru.bjcreslin.naidizakupku.security.entity.Role
 
 @Repository
+@RepositoryRestResource(path = "/api/v1/roles", collectionResourceRel = "roles", itemResourceRel = "role")
 interface RoleRepository: JpaRepository<Role, Long> {
 
     fun findByName(name: String): Role?
--- src/main/kotlin/ru/bjcreslin/naidizakupku/user/repository/UserRepository.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/user/repository/UserRepository.kt
@@ -1,10 +1,12 @@
 package ru.bjcreslin.naidizakupku.user.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.stereotype.Repository
 import ru.bjcreslin.naidizakupku.user.entity.User
 
 @Repository
+@RepositoryRestResource(path = "/api/v1/users", collectionResourceRel = "users", itemResourceRel = "user")
 interface UserRepository : JpaRepository<User, Long> {
 
     fun findByUsername(username: String): User?
