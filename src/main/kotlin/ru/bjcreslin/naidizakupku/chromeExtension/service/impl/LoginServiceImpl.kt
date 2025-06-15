90066d81525d0cacda0dc9abc18456aa2b26b351
BJCreslin <bjcreslin@gmail.com>
2025-06-15 17:51:40 +0700

    добавил код с токеном

--- src/main/kotlin/ru/bjcreslin/naidizakupku/chromeExtension/service/impl/LoginServiceImpl.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/chromeExtension/service/impl/LoginServiceImpl.kt
@@ -1,17 +1,28 @@
 package ru.bjcreslin.naidizakupku.chromeExtension.service.impl
 
+import jakarta.ws.rs.BadRequestException
 import org.springframework.stereotype.Service
 import ru.bjcreslin.naidizakupku.chromeExtension.dto.NumberCodeRequestDto
 import ru.bjcreslin.naidizakupku.chromeExtension.service.LoginService
+import ru.bjcreslin.naidizakupku.codeService.TelegramCodeService
 import ru.bjcreslin.naidizakupku.security.service.JwtTokenProvider
 
 @Service
-class LoginServiceImpl(val jwtTokenProvider: JwtTokenProvider) : LoginService {
+class LoginServiceImpl(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val telegramCodeService: TelegramCodeService
+) : LoginService {
 
     override fun login(codeRequest: NumberCodeRequestDto): String {
         if (codeRequest.numberCode == 1000) {
             return jwtTokenProvider.createAccessToken("user", listOf("user", "admin"))
         }
-        return ""
+
+        val user = telegramCodeService.getUserByCode(codeRequest.numberCode)
+            ?: throw BadRequestException("User not found")
+        return jwtTokenProvider.createAccessToken(
+            user.username,
+            user.userRoles.map { it.role.name }
+        )
     }
 }
