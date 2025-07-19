d866cfbd90937e1882d140dceb12a46cfdd1a41a
BJCreslin <bjcreslin@gmail.com>
2025-07-20 01:36:24 +0700

    Контролер входа для админки

---
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/admin_api/controller/LoginController.kt
@@ -0,0 +1,24 @@
+package ru.bjcreslin.naidizakupku.admin_api.controller
+
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.bjcreslin.naidizakupku.chromeExtension.dto.NumberCodeRequestDto
+import ru.bjcreslin.naidizakupku.chromeExtension.service.LoginService
+
+@RestController
+@RequestMapping("admin/login")
+class LoginController(
+    private val loginService: LoginService
+) {
+
+    @PostMapping("")
+    fun login(
+        @Valid @RequestBody codeRequest: NumberCodeRequestDto
+    ): ResponseEntity<String> {
+        return ResponseEntity.ok().body(loginService.login(codeRequest))
+    }
+}
