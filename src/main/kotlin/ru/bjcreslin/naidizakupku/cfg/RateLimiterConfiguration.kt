28f2031cca1ea7d93c35ab018720db55158a5057
BJCreslin <bjcreslin@gmail.com>
2025-07-20 20:26:50 +0700

    Изменил значение

--- src/main/kotlin/ru/bjcreslin/naidizakupku/cfg/RateLimiterConfiguration.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/cfg/RateLimiterConfiguration.kt
@@ -1,6 +1,5 @@
 package ru.bjcreslin.naidizakupku.cfg
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
@@ -10,9 +9,7 @@ import org.springframework.stereotype.Component
 @ConfigurationProperties(prefix = "rate-limit")
 @Component
 class RateLimiterConfiguration {
-    @Value("\${max-requests}")
     val maxRequests: Int = 10
 
-    @Value("\${period-minutes}")
     val periodMinutes: Long = 1
 }
