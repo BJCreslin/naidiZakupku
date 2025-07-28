6d5d81ebfa54d6d3c3f593077761efd7c17d4eb3
BJCreslin <bjcreslin@gmail.com>
2025-07-28 21:45:58 +0700

    Сохранение только новых новостей

--- src/main/kotlin/ru/bjcreslin/naidizakupku/news/parsers/NewsParsingService.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/news/parsers/NewsParsingService.kt
@@ -3,6 +3,7 @@ package ru.bjcreslin.naidizakupku.news.parsers
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import ru.bjcreslin.naidizakupku.news.dbo.News
 import ru.bjcreslin.naidizakupku.news.dbo.NewsType
 import ru.bjcreslin.naidizakupku.news.repository.NewsRepository
@@ -16,11 +17,12 @@ class NewsParsingService(val newsRepository: NewsRepository, val rssService: Rss
     private val baseUrl = "https://zakupki.gov.ru"
 
     @Scheduled(fixedRate = 3_600_000) // Каждый час
+    @Transactional
     fun parseNews() {
         try {
             logger.info("Starting news parsing...")
 
-            val newsItems = rssService.loadNews(LocalDate.now().minusDays(1))
+            val newsItems = rssService.loadNews(LocalDate.now().minusDays(20))
                 .map { newsItem ->
                     News(
                         title = newsItem.title, url = baseUrl + newsItem.link, publicationDate = newsItem.publishedAt,
@@ -28,8 +30,12 @@ class NewsParsingService(val newsRepository: NewsRepository, val rssService: Rss
                         newsType = NewsType.GENERAL
                     )
                 }
+            val savedNewsItems = newsRepository.findAll()
 
-            val savedCount = saveNewNews(newsItems)
+            val newNewsItems =
+                newsItems.filter { newsItem -> savedNewsItems.none { saved -> saved.url == newsItem.url } }.toList()
+
+            val savedCount = saveNewNews(newNewsItems)
 
             logger.info("Parsing completed. Saved $savedCount new items")
         } catch (e: Exception) {
