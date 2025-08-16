6ff50b1326e37449bc2f70848a33841582642c34
BJCreslin <bjcreslin@gmail.com>
2025-08-17 00:00:29 +0700

    Убрал лишнюю зависимость

--- src/main/kotlin/ru/bjcreslin/naidizakupku/controller/ProcurementResource.kt
+++ src/main/kotlin/ru/bjcreslin/naidizakupku/controller/ProcurementResource.kt
@@ -1,39 +1,21 @@
 package ru.bjcreslin.naidizakupku.controller
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.domain.Specification
-import org.springframework.data.web.PagedModel
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import ru.bjcreslin.naidizakupku.cfg.CustomMetricsService
 import ru.bjcreslin.naidizakupku.procurement.dto.ProcurementListResponse
 import ru.bjcreslin.naidizakupku.procurement.filter.ProcurementFilter
-import ru.bjcreslin.naidizakupku.procurement.mapper.ProcurementMapper
-import ru.bjcreslin.naidizakupku.procurement.entity.Procurement
-import ru.bjcreslin.naidizakupku.procurement.repository.ProcurementRepository
+import ru.bjcreslin.naidizakupku.procurement.service.ProcurementService
 import ru.bjcreslin.naidizakupku.security.dto.JwtUser
 import ru.bjcreslin.naidizakupku.user.entity.User
 import ru.bjcreslin.naidizakupku.user.repository.UserRepository
-import java.io.IOException
-import java.util.Optional
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import ru.bjcreslin.naidizakupku.cfg.CustomMetricsService
-import ru.bjcreslin.naidizakupku.procurement.service.ProcurementService
 
 @RestController
 @RequestMapping("/api/v1/procurements")
